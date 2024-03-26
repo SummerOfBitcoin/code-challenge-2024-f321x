a3c95f59529e4dec10dd775968dc59b4cab6f0b1
fbock <fbock@student.42heilbronn.de>
2024-03-26 14:51:17 +0000

    cargo new

---
+++ solution/code-challenge-f321x/target/debug/code-challenge-f321x.d
@@ -0,0 +1,1 @@
+/workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/target/debug/code-challenge-f321x: /workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/src/main.rs
