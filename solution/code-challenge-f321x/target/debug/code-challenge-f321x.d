ec1624fc6a1cc1ac2f2797d5253c56b5f4d81cd7
fbock <fbock@student.42heilbronn.de>
2024-03-26 14:54:02 +0000

    gitignore

--- solution/code-challenge-f321x/target/debug/code-challenge-f321x.d
+++
@@ -1,1 +0,0 @@
-/workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/target/debug/code-challenge-f321x: /workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/src/main.rs
--- solution/code-challenge-f321x/target/debug/deps/code_challenge_f321x-0a45d6e84d366cf5.d
+++
@@ -1,5 +0,0 @@
-/workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/target/debug/deps/code_challenge_f321x-0a45d6e84d366cf5: src/main.rs
-
-/workspaces/code-challenge-2024-f321x/solution/code-challenge-f321x/target/debug/deps/code_challenge_f321x-0a45d6e84d366cf5.d: src/main.rs
-
-src/main.rs:
