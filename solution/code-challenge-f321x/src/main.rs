3e0b1feb1e5966cc61e88c5102e30003ef3996bd
fbock <fbock@student.42heilbronn.de>
2024-03-27 16:23:28 +0000

    parsing

--- solution/code-challenge-f321x/src/main.rs
+++ solution/code-challenge-f321x/src/main.rs
@@ -1,8 +1,16 @@
 pub mod parsing;
 
 fn main() {
-    // load transactions,
-    // verify transactions, filter out invalid ones
-    println!("Hello, world!");
-    parsing::parse_transactions_from_dir("/workspaces/code-challenge-2024-f321x/mempool")
+    let parsed_transactions = parsing::parse_transactions_from_dir("/workspaces/code-challenge-2024-f321x/mempool");
+
+    for tx in &parsed_transactions {
+		println!("{:?}\n\n", tx);
+        // for tx_out in &tx.vout {
+        //     if let scriptpubkey_type = &tx_out.scriptpubkey_type {
+        //         if scriptpubkey_type == "nulldata" {
+        //             std::process::exit(0);
+        //         }
+        //     }
+        // }
+	}
 }
--- solution/code-challenge-f321x/src/parsing/mod.rs
+++ solution/code-challenge-f321x/src/parsing/mod.rs
@@ -1,21 +1,48 @@
+// functions to load the Json files from the specified directory in a Vec of Json objects (json crate)
+
+pub mod transaction_structs;
+pub mod json_parsing;
+
 use std::fs;
 
-pub fn read_whole_directory(directory_path: &str) -> Vec<String> {
-	let	mut files: Vec<String> = Vec::new();
+use self::transaction_structs::Transaction;
+use serde_json::from_str;
 
-    for file in fs::read_dir(directory_path).expect("Failed to read directory!") {
-		let dir = file.expect("Failed to read entry!");
-
-		let path_str = dir.path().to_str().expect("Conversion to str failed!");
-		files.push(path_str.to_string());
-    }
-	files
+fn parse_json(str_content: &str) -> Option<Transaction> {
+	let tx = from_str::<Transaction>(str_content);
+	if tx.is_ok() {
+		return Some(tx.expect("Error in unwrapping deserialized json"));
+	} else {
+		println!("{:#?}", tx.err());
+	}
+	None
 }
 
-pub fn parse_transactions_from_dir(directory: &str) -> () {
-	let mut transactions: Vec<String> = read_whole_directory(directory);
-
-	for tx in &transactions {
-		println!("{}", tx);
+fn parse_file_content(file_to_load: fs::DirEntry) -> Option<Transaction> {
+	let file_path_buf = file_to_load.path();
+	if file_path_buf.extension().expect("Invalid file extension") != "json" {
+		println!("Invalid file extension: {}, continuing...", file_path_buf.as_path().display());
+		return None;
 	}
+	let file_content = fs::read_to_string(file_path_buf.as_path()).expect("Reading file content failed");
+
+	match parse_json(&file_content) {
+		Some(tx) => Some(tx),
+		None => {
+			panic!("Invalid Json content in file: {:?}, Delete or correct this file!\n", file_path_buf);
+		}
+	}
+
+}
+
+pub fn parse_transactions_from_dir(directory_path: &str) -> Vec<Transaction> {
+	let	mut transactions: Vec<Transaction> = Vec::new();
+
+    for file in fs::read_dir(directory_path).expect("Failed to read directory!") {
+		let dir_entry = file.expect("Failed to read file entry!");
+		if let Some(transaction) = parse_file_content(dir_entry) {
+			transactions.push(transaction);
+		}
+    }
+	transactions
 }
